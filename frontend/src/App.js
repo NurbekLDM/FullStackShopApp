01b2dd20c2c718961e3c014098fc8851b74c4ae9
NurbekLDM <jumaboyevabarno80@gmail.com>
2025-02-01 01:32:33 +0500

    Adding some admin panel's page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {Suspense} from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import RegisterPage from "./pages/register";
 import './App.css';
-import ReactDOM from "react-dom/client";
 import LoginPage from "./pages/login";
 import HomePage from "./pages/home";
 import ProductDetails from './components/productDetails';
@@ -11,26 +10,44 @@ import Payment from './pages/payment';
 import OrdersPage from "./pages/orders";
 import OrderDetails from "./pages/orderDetails";
 import Favourites from "./pages/favourites";
+import Dashboard from "./pages/adminPanel/dashboard";
+import ProductsPage from "./pages/adminPanel/components/products";
 
 export default function App() {
+
+  const routes= [
+    { path:"/", element:<HomePage /> },
+    { path:"/register", element:<RegisterPage /> },
+    {path:"/login", element:<LoginPage /> },
+    { path:"/product", element:<ProductDetails /> },
+    { path:"/checkout", element:<CheckoutPage />},
+    { path:"/payment", element:<Payment />},
+    { path:"/orders", element:<OrdersPage />},
+    { path:"/orderDetails", element:<OrderDetails />},
+    { path:"/favourites", element:<Favourites />},
+    {
+      path: "adminPanel",
+      element: <Dashboard />,
+      children : [
+        {path: "products", element: <ProductsPage />},
+      ],
+    }
+
+  ]
+
+  const router = createBrowserRouter(routes, {
+    future: {
+        v7_startTransition: true,
+    }
+  })
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path= "/login" element={<LoginPage />} />
-        <Route path="/product" element={<ProductDetails />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/orderDetails" element={<OrderDetails />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </BrowserRouter>
+      <Suspense fallback={<div style={{color: 'white', textAlign: 'center', marginTop: '20%'}}>Loading...</div>}>
+        <RouterProvider router={router}/>
+      </Suspense>
+
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
 
 
--- frontend/src/pages/adminPanel/dashboard.jsx
+++ frontend/src/pages/adminPanel/dashboard.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import AdminPanelNavbar from "./components/navbar";
+import {Outlet , useLocation} from "react-router-dom";
+import {ClipLoader} from 'react-spinners';
 
 export default function Dashboard() {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData('Loaded data');
+            setLoading(false);
+        }, 2000);
+    }, []);
+    const location = useLocation()
+    const isDashboard = location.pathname === '/adminPanel';
     return (
         <div>
-            <h1>Dashboard</h1>
+            <AdminPanelNavbar />
+
+            <div>
+                {loading ? (
+                    <div style={{marginLeft: '50%', marginTop: '250px'}} className="spinner">
+                        <ClipLoader size={50} color="#123abc" loading={loading}/>
+                    </div>
+                ) : (
+                    <div className="content">
+                        {isDashboard}
+                        <Outlet/>
+                    </div>
+                )}
+            </div>
+
         </div>
     );
 }
